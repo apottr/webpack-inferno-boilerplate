b32313745882be1e7ad4da4a072f3c118b412eed
Amelia <apottr@akira.local>
2017-03-18 20:03:16 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Inferno from 'inferno'
+
+function MyComponent(props){
+  return (<h1>Hello, world!</h1>)
+}
+
+Inferno.render(<MyComponent />,document.getElementById('app'))
+
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	entry: './src/main.js',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+			}
+			]
+		}
+}
+
